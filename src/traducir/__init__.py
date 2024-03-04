b0865896dab4428986b5db61c919abd9f98326a5
Nicholas Witthoeft <njwitthoeft@gmail.com>
2024-03-03 23:02:31 -0600

    add pre-commit and mypy

--- src/traducir/__init__.py
+++ src/traducir/__init__.py
@@ -1,2 +1,3 @@
-def hello() -> str:
-    return "Hello from traducir!"
+from .console import console
+
+__all__ = ["console"]
--- src/traducir/tui.py
+++ src/traducir/tui.py
@@ -1,10 +1,12 @@
 """Traducir, a TUI to make gdal_translate intuitive to use."""
-from rich import print
+
+from traducir.console import console
 
 
-def tui():
+def tui() -> None:
     """TUI Entrypoint."""
-    print("Hola Traducir!")
+    console.print("Hola Traducir!")
+
 
 if __name__ == "__main__":
     tui()
