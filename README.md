7cfce13eac9801dc80331e721a17f502b0f0cff5
Nicholas Witthoeft <njwitthoeft@gmail.com>
2024-03-03 22:56:19 -0600

    rye init

---
+++ README.md
@@ -0,0 +1,3 @@
+# traducir
+
+gdal_translate TUI frontend.
---
+++ src/traducir/tui.py
@@ -0,0 +1,10 @@
+"""Traducir, a TUI to make gdal_translate intuitive to use."""
+from rich import print
+
+
+def tui():
+    """TUI Entrypoint."""
+    print("Hola Traducir!")
+
+if __name__ == "__main__":
+    tui()
